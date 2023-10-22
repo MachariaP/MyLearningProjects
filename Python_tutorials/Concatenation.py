6cb3fd7739235cf88fa61deb2581b0d1c5921b86
MachariaP <walburphinehas78@gmail.com>
2023-10-22 09:07:44 +0300

    Created the String_Methods python file

--- Python_tutorials/Concatenation.py
+++ Python_tutorials/Concatenation.py
@@ -1,14 +1,22 @@
-    # Python - String Concatenation
+# Python - String Concatenation
 
-#String Concatenation (Combine)
-#use the + operator
+# String Concatenation (Combine)
+# use the + operator
 # (Merge variable a with variable b into variable c :)
 a = "Hello"
 b = " World"
 c = a + b
 print(c)
-#To add a space between them, add a " ":
+# To add a space between them, add a " ":
 a = "Hello"
 b = "World"
 c = a + " " + b
 print(c)
+
+a = "Coding"
+b = "is"
+c = "crazy"
+d = a + b + c
+e = a + " " + b + " " + c
+print(d)
+print(e)
---
+++ Python_tutorials/StringMethods.py
@@ -0,0 +1,16 @@
+# STRING METHODS
+
+# 1. Capitalize()
+# Converts the first character to upper case
+x = "hello, World!"
+print(x.capitalize())  # returns Hello, World!
+
+# 2. casefold()
+# converts string into lower case
+x = "HELLO, World!"
+print(x.casefold())  # returns hello, world!
+
+# 3. center()
+# Returns a centered string
+x = "Hello, World!"
+print(x.center())
