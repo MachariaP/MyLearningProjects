8b5893db4a0b87ab4a83dc187104b4a638935c62
MachariaP <walburphinehas78@gmail.com>
2023-10-22 09:41:23 +0300

    modified:   pythonBooleans.py

--- Python_tutorials/pythonBooleans.py
+++ Python_tutorials/pythonBooleans.py
@@ -25,3 +25,37 @@ y = 15
 
 print(bool(x))
 print(bool(y))
+
+# Most Values are True
+# Almost any value is evaluated to True if it has some sort of content.
+
+# Any string is True, except empty strings.
+
+# Any number is True, except 0.
+
+# Any list, tuple, set, and dictionary are True, except empty ones.
+
+# The following will return False:
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+# If you have an object that is made from a class with a __len__ function that returns 0 or False, it evaluates to False:
+
+
+class myclass:
+    def _len_(self):
+        return 0
+
+
+myobj = myclass()
+print(bool(myobj))
+
+# isinstance()
+# is a python builtin function that returns a boolen value
+# can be used to determine if any object is of certain data type
+x = 200
+print(isinstance(x, int))
