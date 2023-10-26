f766fa10c28858d9dca6f27b6f6bb450eb6dcc0d
MachariaP <walburphinehas78@gmail.com>
2023-10-26 15:19:05 +0300

    Created the execute file

--- c-programming/simple_shell/main.c
+++ c-programming/simple_shell/main.c
@@ -10,10 +10,9 @@
 int main(int ac, char **argv)
 {
     char *line = NULL, **command = NULL; /* Buffer to store the input line */
-    int i, status = 0;                   /* Status of the shell */
+    int status = 0;                      /* Status of the shell */
 
     (void)ac;
-    (void)argv;
 
     while (1)
     {
@@ -31,13 +30,6 @@ int main(int ac, char **argv)
         if (!command)
             continue;
 
-        for (i = 0; command[i]; i++)
-        {
-            printf("%s\n", command[i]);
-            free(command[i]), command[i] = NULL;
-        }
-        free(command), command = NULL;
-
-        /* status = _execute(command, argv); */
+        status = _execute(command, argv);
     }
 }
