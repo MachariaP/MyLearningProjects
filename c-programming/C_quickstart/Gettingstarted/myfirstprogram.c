ec67913f2487544bd33034270abf4cb13e9f0e88
MachariaP <walburphinehas78@gmail.com>
2023-11-12 12:15:12 +0300

    Created and performed modifications in myfirstprogram file

--- c-programming/C_quickstart/Gettingstarted/myfirstprogram.c
+++ c-programming/C_quickstart/Gettingstarted/myfirstprogram.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
 
-int main()
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("Hello World!\n");
-	return 0;
+	/* Print "Hello World!" followed by two newlines */
+	printf("Hello World!\n\n");
+
+	/* Print "I am learning C." */
+	printf("I am learning C.");
+
+	/*
+	 * Print "Hello World!" followed by a newline,
+	 * "I am learning C.", and "And it is awesome!"
+	 */
+	printf("Hello World!\nI am learning C.\nAnd it is awesome!");
+
+	/* Return 0 to indicate successful completion of the program */
+	return (0);
 }
