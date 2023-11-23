3afd9bcf2036d3706f0e13d9d754eb3d7fc18ef8
MachariaP <walburphinehas78@gmail.com>
2023-11-23 12:33:53 +0300

    modified file variables.py to add other variables

--- Python_tutorials/datacamp/variables.py
+++ Python_tutorials/datacamp/variables.py
@@ -27,3 +27,27 @@ intro = "Hello! How are you?"
 
 # Create a variable is_good
 is_good = True
+
+# operations with other types
+
+"""
+Calculate the product of monthly_savings and num_months. Store the result in year_savings.
+What do you think the resulting type will be? Find out by printing out the type of year_savings.
+Calculate the sum of intro and intro and store the result in a new variable doubleintro.
+Print out doubleintro
+"""
+monthly_savings = 10
+num_months = 12
+intro = "Hello! How are you?"
+
+# Calculate year_savings using monthly_savings and num_months
+year_savings = monthly_savings * num_months
+
+# Print the type of year_savings
+print(type(year_savings))
+
+# Assign sum of intro and intro to doubleintro
+doubleintro = intro + intro
+
+# Print out doubleintro
+print(doubleintro)
