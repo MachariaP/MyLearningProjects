6c0314d4f5e3c44d5982a755bad801786a8a2717
MachariaP <walburphinehas78@gmail.com>
2023-11-23 12:17:02 +0300

    modified file variables.py to add other variables

--- Python_tutorials/datacamp/variables.py
+++ Python_tutorials/datacamp/variables.py
@@ -17,3 +17,13 @@ total_savings = new_savings + savings
 # Print total_savings
 print(total_savings)
 
+#other variables 
+
+# Create a variable half
+half = 0.5
+
+# Create a variable intro
+intro = "Hello! How are you?"
+
+# Create a variable is_good
+is_good = True
