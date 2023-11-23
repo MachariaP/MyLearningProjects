924e2e54e8409ca7a1881a0cbab29348f9ec3bba
MachariaP <walburphinehas78@gmail.com>
2023-11-23 10:51:53 +0300

    created the math_operations.py file

---
+++ Python_tutorials/datacamp/math_operations.py
@@ -0,0 +1,18 @@
+# math_operations.py
+
+# Print the sum of 4 + 5
+sum_result = 4 + 5
+print("Sum:", sum_result)
+
+# Print the result of subtracting 5 from 5
+subtract_result = 5 - 5
+print("Subtraction:", subtract_result)
+
+# Multiply 3 by 5
+multiply_result = 3 * 5
+print("Multiplication:", multiply_result)
+
+# Divide 10 by 2
+divide_result = 10 / 2
+print("Division:", divide_result)
+
