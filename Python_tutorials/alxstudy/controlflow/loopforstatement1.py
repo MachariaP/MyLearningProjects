df3e6815e4fe6ab1d790e1d382583da09a2b0ad7
MachariaP <walburphinehas78@gmail.com>
2023-11-28 11:07:26 +0300

    run python controlflow loop scripts

--- Python_tutorials/alxstudy/controlflow/loopforstatement1.py
+++ Python_tutorials/alxstudy/controlflow/loopforstatement1.py
@@ -4,4 +4,21 @@
 numbers = [1, 2, 3, 4, 5]
 new_numbers = [num for num in numbers if num % 2 != 0]
 
-print(new_numbers)  # This will give the expected result without modifying the original list
+# print(new_numbers)  # This will give the expected result without modifying the original list
+
+# create a sample collection
+users = {'Hans': 'active','Eleonore':'inacrive', 'James': 'active'}
+
+# strategy: Iterate over a copy
+for user, status in users.copy().items():
+    if status == 'inactive':
+        del users[user]
+
+# strategy: create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+print(new_numbers)
+print(active_users)
