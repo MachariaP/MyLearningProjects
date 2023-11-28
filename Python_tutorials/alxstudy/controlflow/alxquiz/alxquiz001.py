2a8e2597feed44e6f39117c09df1707b7bdc001a
MachariaP <walburphinehas78@gmail.com>
2023-11-28 11:32:18 +0300

    run python controlflow loop scripts

---
+++ Python_tutorials/alxstudy/controlflow/alxquiz/alxquiz001.py
@@ -0,0 +1,64 @@
+#!usr/bin/python3
+
+# What do these lines print
+a = 12
+if a > 2:
+    if a % 2 ==0:
+        print("Holberton")
+    else:
+        print("C is fun")
+else:
+    print("school")         
+
+# quiz 1
+    
+if 12 == 48/3 or 12 is 12:
+    print("Holberton")
+else:
+    print("School")
+
+# quiz 2
+
+a = 12
+if a < 2:
+    print("Holberton")
+elif a % 2 == 0:
+    print("C is fun")
+else:
+    print("School")
+
+# quiz 3
+
+for i in range(2, 10, 2):
+    print(i, end=" ")
+
+# quiz 4
+
+for i in range(4):
+    print(i, end=" ")
+
+# quiz 5                      
+
+if True:
+    print("Holberton")
+else:
+    print("School")
+
+# quiz 6
+
+if 12 == 48/4 and False:
+    print("Holberton")
+else:
+    print("School")
+
+# quiz 7
+
+if 12 == 48/4:
+    print("Holberton")
+else:
+    print("School")
+
+# quiz 8
+
+for i in range(2, 4):
+    print(i, end=" ")
