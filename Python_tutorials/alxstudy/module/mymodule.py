2060bc4f7c9dea4f51c4f11dfb16e5448d18ab8e
MachariaP <walburphinehas78@gmail.com>
2023-11-30 11:44:52 +0300

    Import a simple function from a simple file

---
+++ Python_tutorials/alxstudy/module/mymodule.py
@@ -0,0 +1,9 @@
+# mymodule.py
+
+"""
+a module is the same as a code library
+contains a set of functions you want to include in your application
+"""
+# creating a module
+def greeting(name):
+    print(f"Hello, {name}")
