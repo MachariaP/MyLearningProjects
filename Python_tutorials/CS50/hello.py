d477193193757656d021edd631cc9d7e41e12ac1
MachariaP <walburphinehas78@gmail.com>
2023-12-05 20:14:34 +0300

    modified a function that finds the biggest integer of a list. file

--- Python_tutorials/CS50/hello.py
+++ Python_tutorials/CS50/hello.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
-
+"""
 name = input("What's your name? ")
 print("'Hello', ", end="")
 print(name)
+"""
+"""
+print('Hello, "friend"')
+
+print("Hello, \"friend\"")
+"""
+
+# Ask user for their name
+name = input("What's your name? ")
+
+# say hello to user
+print(f"hello, {name}")
