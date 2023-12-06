f57c39bdb858acef98ac003d3ddb6455a78e7388
MachariaP <walburphinehas78@gmail.com>
2023-12-06 10:44:08 +0300

    practiced Lambda and map() function and run python scripts

--- Python_tutorials/alxstudy/Lambda/map.py
+++ Python_tutorials/alxstudy/Lambda/map.py
@@ -6,9 +6,26 @@
 (i) a function (`func`) and 
 (ii) a sequence (like a list).
 """
-# How `map()` work?
+# How `map()` works?
 """
 - `map(func, seq)` applies the function `func` to each element in the sequence `seq`.
 - it returns an iterator.
-
 """
+# Example without Lambda 
+def fahrenheit(T):
+    return ((float(9)/5)*T + 32)
+
+def celsius(T):
+    return (float(5)/9)*(T-32)
+
+temperatures = (36.5, 37, 37.5, 38, 39)
+F = map(fahrenheit, temperatures)
+C = map(celsius, F)
+
+# Convert iterators to lists for printing
+temperatures_in_Fahrenheit = list(F)
+temperatures_in_Celsius = list(C)
+
+print(temperatures_in_Fahrenheit)
+print(temperatures_in_Celsius)
+
