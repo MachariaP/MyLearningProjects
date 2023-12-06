c4a2282d4d1cf18afa5566493533b425af39ab76
MachariaP <walburphinehas78@gmail.com>
2023-12-06 10:18:23 +0300

    practiced Lambda and map() function and run python scripts

---
+++ Python_tutorials/alxstudy/Lambda/map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# What is `map()`?
+"""
+`map()` - is a function in python that takes two arguments:
+(i) a function (`func`) and 
+(ii) a sequence (like a list).
+"""
+# How `map()` work?
+"""
+- `map(func, seq)` applies the function `func` to each element in the sequence `seq`.
+- it returns an iterator.
+
+"""
