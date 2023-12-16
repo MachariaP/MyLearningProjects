79eb5c529b8ae4406e4bc35032d4c389b0d88d1a
MachariaP <walburphinehas78@gmail.com>
2023-12-16 14:20:22 +0300

    compiled c# code

---
+++ c#/helloworld.cs
@@ -0,0 +1,15 @@
+// helloworld.cs
+
+using System;
+
+class program
+{
+	static void Main()
+	{
+		Console.WriteLine("Hello World!");
+
+		Console.Write("Congratulations!");
+		Console.Write(" ");
+		Console.Write("You wrote your first line of code.\n");
+	}
+}
