03f1ed0b7d3629e6109244584dc8fe35e9a12686
MachariaP <walburphinehas78@gmail.com>
2023-12-24 00:23:29 +0300

    run a python script that concatenate two strings

---
+++ Python_tutorials/murach_python/full_name.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This script demonstrates the creation of a full name by combining first and last names.
+
+Usage:
+1. Set the `first_name` variable to the desired first name.
+2. Set the `last_name` variable to the desired last name.
+3. The script will create a `full_name` by concatenating `first_name` and `last_name`.
+4. The result will be printed to the console.
+
+Example:
+first_name = "Ada"
+last_name = "Lovelace"
+full_name = f"{first_name} {last_name}"
+print(full_name)  # Output: Ada Lovelace
+"""
+
+first_name = "Ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+
+print(full_name)
+
