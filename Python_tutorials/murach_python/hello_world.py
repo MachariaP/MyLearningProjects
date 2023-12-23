1e2b1b64c42a07947c2556edcb145313c13233ef
MachariaP <walburphinehas78@gmail.com>
2023-12-23 23:54:50 +0300

    run a python script that outputs 2 messages

---
+++ Python_tutorials/murach_python/hello_world.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Declearing a variable 
+message = "Hello Python World!"
+
+print(message)
+
+# Declear another variable
+message = "Hello Python Crash Course World!"
+
+print(message)
