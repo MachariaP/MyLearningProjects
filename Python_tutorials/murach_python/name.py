41e83265585293509a5b9316727988a41a57e870
MachariaP <walburphinehas78@gmail.com>
2023-12-24 00:15:50 +0300

    run a python script that outputs a string

---
+++ Python_tutorials/murach_python/name.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This script demonstrates changing the case of a string using string methods.
+
+1. The `title()` method capitalizes the first letter of each word.
+2. The `upper()` method converts the entire string to uppercase.
+3. The `lower()` method converts the entire string to lowercase.
+"""
+
+# Example usage
+name = "ada lovelace"
+print(name.title())
+
+name = "Ada Lovelace"
+print(name.upper())
+print(name.lower())
+
