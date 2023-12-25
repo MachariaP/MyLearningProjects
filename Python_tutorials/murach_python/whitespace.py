d1a6a1dcd7c51bbe289fd96828b52da3d25f7e2d
MachariaP <walburphinehas78@gmail.com>
2023-12-25 10:48:23 +0300

    created the whitespace file

---
+++ Python_tutorials/murach_python/whitespace.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# adding whitespace to strings with Tabs or Newlines
+
+'''
+    white space refers to any nonprinting characters ie space,
+    tabs and end-of-line symbols.
+    \t - for adding a tab to a text
+    \n - for adding a new line to a string
+
+'''
+# adding a tab \t
+print("Python")
+print("\tPython")
+
+# adding a newline \n
+print("Languages:\nPython\nC\nJavascript")
+
+# combining newline and tab together \n\t
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+# Stripping Whitespace
+
+'''
+    Python can detect whitespace on the right or left side of a string.
+    To ensure that no whitespace exsists at the right end
+    of a string, use the rstrip() method
+
+'''
+print()
+language = 'Python '
+print(language)
+print()
+print(language.rstrip())
+print()
+print(language)
