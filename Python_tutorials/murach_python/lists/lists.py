ea4331d16ee38edf33e4ae90d9c3edfe8fd98bcb
MachariaP <walburphinehas78@gmail.com>
2023-12-25 12:43:14 +0300

    introduction to python lists.

---
+++ Python_tutorials/murach_python/lists/lists.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+'''
+    a list - a collection of items in a prticular order
+    ([]) - a square brackets indicates a list.
+    Elements are separated by commas.
+
+'''
+
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+
+print()
+# Accessing Elements in a list
+''' 
+    To access an element in a list:
+        write name of the list
+        followed by the indexof the item enclosed in square brackets
+    example below:
+
+'''
+
+print(bicycles[0])
+print(bicycles[3])
---
+++ Python_tutorials/murach_python/variables_data/apostrophe.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Avoiding syntax Errors with Strings
+# How to use single and double quotes correctly.
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+'''
+    If we used single quotes, python couldn't identify where the sting should end and it would display a syntaxError: invalid syntax
+
+'''
