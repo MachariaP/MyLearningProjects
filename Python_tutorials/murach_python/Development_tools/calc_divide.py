30c32c032ef2ce9d673e7ceff43940e7d8eb5354
MachariaP <walburphinehas78@gmail.com>
2024-01-02 13:49:41 +0300

    building more elaborate doctest tests

---
+++ Python_tutorials/murach_python/Development_tools/calc_divide.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Writing doctest Tests for Catching Exceptions
+
+# calculations.py
+# ...
+
+def divide(a, b):
+    """Compute and return the quotient of two numbers.
+
+    Usage examples:
+    >>> divide(84, 2)
+    42.0
+    >>> divide(15, 3)
+    5.0
+    >>> divide(42, -2)
+    -21.0
+    >>> divide(42, 0)
+    Traceback (most recent call last):
+    ZeroDivisionError: division by zero
+    """
+    return float(a / b)
