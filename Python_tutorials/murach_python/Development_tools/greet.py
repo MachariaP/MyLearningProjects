a6030c5714ff34ef634452fc4e554b6d36d925b6
MachariaP <walburphinehas78@gmail.com>
2024-01-02 14:42:02 +0300

    dealing with BLANKLINE in doctest python script

---
+++ Python_tutorials/murach_python/Development_tools/greet.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+# Dealing With Whitespaces and Other Characters
+
+# greet.py
+
+def greet(name="World"):
+    """Print a greeting.
+
+    Usage examples:
+    >>> greet("Pythonista")
+    Hello, Pythonista!
+    <BLANKLINE>
+    How have you been?
+    """
+    print(f"Hello, {name}!")
+    print()
+    print("How have you been?")
