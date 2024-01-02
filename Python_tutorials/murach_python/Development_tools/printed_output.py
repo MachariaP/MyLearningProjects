88e35dd54d41854891cfdd0a11b425a700b4d25d
MachariaP <walburphinehas78@gmail.com>
2024-01-02 11:48:11 +0300

    run a printed_output doctest python script

---
+++ Python_tutorials/murach_python/Development_tools/printed_output.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+# printed_output.py
+
+def greet(name="World"):
+    """print a greeting to the screen.
+
+
+    Usage examples:
+    >>> greet("Pythonians")
+    Hello, Pythonians!
+    >>> greet()
+    Hello, World!
+    """
+    print(f"Hello, {name}!")
