211e6ee460eb54f50d8b045a1707f16cdc3c382c
MachariaP <walburphinehas78@gmail.com>
2024-01-05 12:25:37 +0300

    List membership python script

---
+++ Python_tutorials/alxstudy/Lists/list_membership.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""
+Module demonstrating the use of lists.
+
+This module creates a list of horsemen and checks for the presence of certain elements.
+
+Usage:
+    Run the code to create a list of horsemen and check for their presence.
+"""
+
+# Create a list of horsemen
+horsemen = ['war', 'famine', 'pestilence', 'death']
+
+# Check if 'pestilence' is in the list
+print('pestilence' in horsemen)  # Output: True
+
+# Check if 'debauchery' is in the list
+print('debauchery' in horsemen)  # Output: False
